9462b92ac3360fe6f60440310252b05a506c29e7
annzem <annzemlinsky@gmail.com>
2021-06-11 14:20:15 +0200

    add encoding with buffer, input output streams

--- src/main/java/com/company/AESEncryptorLogic.java
+++ src/main/java/com/company/AESEncryptorLogic.java
@@ -1,44 +1,57 @@
 package com.company;
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.AlgorithmParameterSpec;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
-import java.util.Arrays;
-import java.util.Base64;
+        import javax.crypto.*;
+        import javax.crypto.spec.IvParameterSpec;
+        import javax.crypto.spec.PBEKeySpec;
+        import javax.crypto.spec.SecretKeySpec;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.OutputStream;
+        import java.security.InvalidAlgorithmParameterException;
+        import java.security.InvalidKeyException;
+        import java.security.NoSuchAlgorithmException;
+        import java.security.SecureRandom;
+        import java.security.spec.InvalidKeySpecException;
+        import java.security.spec.InvalidParameterSpecException;
+        import java.security.spec.KeySpec;
+        import java.util.Arrays;
+        import java.util.Random;
 
 public class AESEncryptorLogic {
-    public static String keyStr = "qwe123TYU-0";
-    static String initVector = "encryptionIntVec";
-    static byte[] encrypted;
-    static String encryptedStr;
-    static IvParameterSpec ivParameterSpec;
-    static SecretKey secretKeySpec;
-    static String saltEncrypt = "salt";
-    static int iterationsEncrypt = 65536;
+    static int iterationsEncrypt = 32768;
+    static byte[] salt;
+    static int bufSize = 1024;
 
-    public static AlgorithmParameterSpec getIv() {
-        AlgorithmParameterSpec ivSpec;
-        byte[] iv = new byte[16]; //cipher.getBlockSize()
-        new SecureRandom().nextBytes(iv);
-        ivParameterSpec = new IvParameterSpec(iv);
-        return ivParameterSpec;
+    public static byte[] generateSalt(int length) {
+        Random r = new SecureRandom();
+        byte[] salt = new byte[length];
+        r.nextBytes(salt);
+        return salt;
     }
 
-    public static SecretKey getKey() {
-        SecretKeyFactory factoryKeyEncrypt = null;
+    static class Keys {
+        public final SecretKey encryption, authentication;
+
+        public Keys(SecretKey encryption, SecretKey authentication) {
+            this.encryption = encryption;
+            this.authentication = authentication;
+        }
+    }
+
+    public static Keys getKey(int keyLength, char[] password, byte[] salt) {
+        SecretKeyFactory factory;
         try {
-            factoryKeyEncrypt = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            KeySpec spec = new PBEKeySpec(keyStr.toCharArray(), saltEncrypt.getBytes(), iterationsEncrypt, 128);
-            secretKeySpec = new SecretKeySpec(factoryKeyEncrypt.generateSecret(spec).getEncoded(), "AES");
-            return secretKeySpec;
+            factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            KeySpec spec = new PBEKeySpec(password, salt, iterationsEncrypt, keyLength + 8 * 8);
+            SecretKey tmp = null;
+            tmp = factory.generateSecret(spec);
+            byte[] fullKey = tmp.getEncoded();
+            SecretKey authKey = new SecretKeySpec(
+                    Arrays.copyOfRange(fullKey, 0, 8), "AES");
+            SecretKey encKey = new SecretKeySpec(
+                    Arrays.copyOfRange(fullKey, 8, fullKey.length), "AES");
+            return new Keys(encKey, authKey);
+
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (InvalidKeySpecException e) {
@@ -47,43 +60,161 @@ public class AESEncryptorLogic {
         return null;
     }
 
-    public static String encrypt2(String input, AlgorithmParameterSpec ivParameterSpec, SecretKey secretKeySpec) {
+    public static void encrypt(int keyLength,
+                               InputStream fileInputStream,
+                               OutputStream fileOutputStream,
+                               char[] password
+    ) {
+        byte[] salt = generateSalt(16);
+
+        Keys keys = getKey(keyLength, password, salt);
+
+        Cipher cipher = null;
         try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
-
-            encrypted = cipher.doFinal(input.getBytes());
-            String encryptedStr = Base64.getEncoder().encodeToString(encrypted);
-            System.out.println("input = " + Arrays.toString(input.getBytes()));
-            System.out.println("encrypted = " + Arrays.toString(encrypted));
-            return encryptedStr;
-
-        } catch (Exception e) {
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, keys.encryption);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
             e.printStackTrace();
         }
-        return null;
-    }
-
-    public static String decrypt2(String input, AlgorithmParameterSpec ivParameterSpec, SecretKey secretKeySpec){
+        byte[] iv = null;
         try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
-
-            byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(input));
-            String decryptedStr = new String(decrypted);
-            System.out.println(decrypted);
-            return (decryptedStr);
-
-        } catch (Exception e) {
+            iv = cipher.getParameters().getParameterSpec(IvParameterSpec.class).getIV();
+        } catch (InvalidParameterSpecException e) {
             e.printStackTrace();
         }
-        return null;
+
+        try {
+            fileOutputStream.write(keyLength / 8);
+            fileOutputStream.write(salt);
+            fileOutputStream.write(keys.authentication.getEncoded());
+            fileOutputStream.write(iv);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        byte[] buf = new byte[bufSize]; //1024
+        int bytesReadNum = 0;
+        byte[] encrypted = null;
+        while (true) {
+            try {
+                if (!((bytesReadNum = fileInputStream.read(buf)) > 0)) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                encrypted = cipher.update(buf, 0, bytesReadNum);
+                fileOutputStream.write(encrypted);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        try {
+            encrypted = cipher.doFinal();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+        if (encrypted != null) {
+            try {
+                fileOutputStream.write(encrypted);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        getIv();
-        getKey();
-        String encryptResult = encrypt2("qweasd", ivParameterSpec, secretKeySpec);
-        System.out.println(decrypt2(encryptResult, ivParameterSpec, secretKeySpec));
+    public static void decrypt(InputStream fileInputStream,
+                               OutputStream fileOutputStream,
+                               char[] password
+    ) {int keyLength=0;
+        try {
+            keyLength = fileInputStream.read() * 8;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] salt = new byte[16];
+        try {
+            fileInputStream.read(salt);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Keys keys = getKey(keyLength, password, salt);
+        byte[] authRead = new byte[8];
+        try {
+            fileInputStream.read(authRead);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (!Arrays.equals(keys.authentication.getEncoded(), authRead)) {
+            try {
+                throw new InvalidPasswordException();
+            } catch (InvalidPasswordException e) {
+                e.printStackTrace();
+            }
+        }
+        byte[] iv = new byte[16];
+        try {
+            fileInputStream.read(iv);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Cipher cipher = null;
+
+        try {
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, keys.encryption, new IvParameterSpec(iv));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+
+        byte[] buf = new byte[bufSize]; //1024
+        int bytesReadNum = 0;
+        byte[] decrypted = null;
+        while (true) {
+            try {
+                if (!((bytesReadNum = fileInputStream.read(buf)) > 0)) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            decrypted = cipher.update(buf, 0, bytesReadNum);
+            if (decrypted != null) {
+                try {
+                    fileOutputStream.write(decrypted);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                decrypted = cipher.doFinal();
+            } catch (IllegalBlockSizeException e) {
+                e.printStackTrace();
+            } catch (BadPaddingException e) {
+                e.printStackTrace();
+            }
+            if (decrypted != null) {
+                try {
+                    fileOutputStream.write(decrypted);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class InvalidPasswordException extends Exception {
     }
 }
