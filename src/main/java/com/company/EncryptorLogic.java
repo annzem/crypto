c2d6e5932dfe7e8b5e9f993df87c53fc60e2c57b
annzem <annzemlinsky@gmail.com>
2021-07-09 14:58:35 +0200

    delete encryptor logic interface

--- src/main/java/com/company/EncryptorLogic.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public interface EncryptorLogic {
-    void encrypt(boolean encrypt, InputStream inputStream, OutputStream outputStream, char[] password) throws ReadException, WriteException;
-}
